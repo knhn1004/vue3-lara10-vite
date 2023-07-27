e30726dcf1be9f20fb74f7116b50e938a955fa42
knhn1004 <kunhan945@gmail.com>
2023-07-26 22:33:06 -0700

    Init vue blade

---
+++ resources/js/app.js
@@ -0,0 +1,5 @@
+import "./bootstrap";
+import { createApp } from "vue";
+import PostsIndex from "./components/Posts/Index.vue";
+
+createApp({}).component("PostsIndex", PostsIndex).mount("#app");
---
+++ resources/js/components/Posts/Index.vue
@@ -0,0 +1,1 @@
+<template>Table coming soon.</template>
